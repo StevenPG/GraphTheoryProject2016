8dc7e5c53bdfa6a0fa7826d0adcb50ab24a5edcc
Steve <steven.p.gantz@gmail.com>
2016-03-16 13:50:50 -0400

    Started drawing image

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
 function setup() {
 
+  // Create the canvas to be drawn on
+  createCanvas(windowWidth, windowHeight);
+
+  // Define image
+  backgroundImage = loadImage("assets/GraphTheoryImage.jpg");
+
 }
 
 function draw() {
+
+  // Draw the background image as the lowest layer on each draw frame
+  image(backgroundImage, 0, 0, width, height);
 
 }
